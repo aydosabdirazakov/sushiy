788dcc438400bd7f5c5f172f5be79eaef3efdf62
Aydos Abdirazakov <136689421+aydosabdirazakov@users.noreply.github.com>
2023-08-04 09:40:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.item_btn').slick({
+        dots: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 200,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        fadeSpeed: 1000
+        
+    });
+  });
